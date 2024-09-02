70decc0ea06bd1c951d6717789a7003f6768e0d9
Taha Shah <tahashah411@gmail.com>
2024-09-02 15:18:49 +0500

    redux for fractions

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { redirect } from 'next/navigation'
+import Link from "next/link";
 
 export default function Home() {
-  
-  redirect('/playground/baseten')
-  
+    
   return (
-    <></>
+    <div className="flex h-screen justify-center items-center gap-2">
+      <Link className="p-3 rounded-sm text-xl bg-red-500 text-white" href="/playground/baseten">Base 10 Blocks</Link>
+      <Link className="p-3 rounded-sm text-xl bg-red-500 text-white" href="/playground/fractions">Fraction Board</Link>
+    </div>
   );
 }
--- lib/redux/hooks.ts
+++ lib/redux/hooks.ts
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector, useStore } from "react-redux"
-import { AppDispatch, RootState, AppStore, selectBaseTenState } from "@/lib/redux/store"
+import { AppDispatch, RootState, AppStore, selectBaseTenState, selectFractionState } from "@/lib/redux/store"
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
@@ -7,3 +7,4 @@ export const useAppSelector = useSelector.withTypes<RootState>()
 export const useAppStore = useStore.withTypes<AppStore>()
 
 export const BaseTenState = () => useAppSelector(selectBaseTenState)
+export const FractionState = () => useAppSelector(selectFractionState)
